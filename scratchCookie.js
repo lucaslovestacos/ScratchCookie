0b7e6e075dcc9d4f1ba890f6d09ecc36e7c8a00a
Lucas Pratt <ljpratt04@gmail.com>
2021-11-12 17:52:38 -0500

    Create scratchCookie.js

---
+++ scratchCookie.js
@@ -0,0 +1,40 @@
+class scratchCookie {
+    constructor() {
+    }
+    
+    getInfo() {
+        return {
+            "id": "Cookie",
+            "name": "Cookie",
+            "blocks": [
+                        {
+                            "opcode": "setCookie",
+                            "blockType": "command",
+                            "text": "set this website's cookies to [cookie]",
+                            "arguments": {
+                                "cookieSet": {
+                                    "type": "string",
+                                    "defaultValue": "Hello, Cookies!"
+                                },
+                            }
+                        },
+                        {
+                            "opcode": "getCookie",
+                            "blockType": "reporter",
+                            "text": "this website's cookie"
+                        },
+                ],
+        };
+    }
+    
+    setCookie({cookieSet}) {
+       document.cookie = cookieSet;
+    }
+    
+    getCookie() {
+        return document.cookie;
+        }
+    }
+}
+
+Scratch.extensions.register(new ScratchFetch())
